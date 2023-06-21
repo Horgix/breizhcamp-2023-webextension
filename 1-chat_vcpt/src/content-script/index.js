7b62891abf27d5225f6cf57662808c145265b5f7
Aurélien Partiot <aurelien.partiot@orange.com>
2023-06-21 14:12:49 +0200

    update: Lot of things

--- 1-chat_vcpt/src/content-script/index.js
+++ 1-chat_vcpt/src/content-script/index.js
@@ -1,33 +1,45 @@
 const dinosettaStone = {
-    add: ["a", "e", "i", "o", "u"],
-    replace: ["th", "ph"]
+    add: ['a', 'e', 'i', 'o', 'u'],
+    replace: ['th', 'ph', 'ch']
 }
 
-function wordToDino(word) {
-    const replaceRegexp = new RegExp("^("+dinosettaStone.replace.join("|")+")", "i")
-    const addRegexp = new RegExp("^("+dinosettaStone.add.join("|")+")", "i")
-
-    if (replaceRegexp.test(word)) 
-        return word.replace(replaceRegexp, "gr")
-    else if (addRegexp.test(word))
-        return word.toLowerCase().replace(addRegexp, "grr$1")
-    else if (/^[1-9]/.test(word))
-        return word
-    else return "gr" + word.slice(1)
-}
-
-function paragrapheToDino(node) {
-    console.log(node.textContent)
-    if (node.textContent.length === 0) return node
-    newP = []
-    for (word of node.textContent.split(/\s/)) {
-        if (word.length === 0) continue
-        translatedWord = wordToDino(word)
-        newP.push(translatedWord)
+function getTextNode (node, action) {
+    if (node.nodeType === Node.TEXT_NODE) {
+        action(node)
+    } else {
+        node.childNodes.forEach(childNode => getTextNode(childNode, action))
     }
-    newP.push("\n")
-    newP[0] = newP[0].slice(0, 1).toUpperCase() + newP[0].slice(1)
-    node.textContent = newP.join(" ")
 }
 
-document.querySelectorAll("p").forEach(paragrapheToDino)
+function wordToDino (word) {
+    const replaceRegexp = new RegExp('^(' + dinosettaStone.replace.join('|') + ')', 'i')
+    const addRegexp = new RegExp('^(' + dinosettaStone.add.join('|') + ')', 'i')
+
+    if (replaceRegexp.test(word)) { return word.replace(replaceRegexp, 'gr') } else if (addRegexp.test(word)) { return word.toLowerCase().replace(addRegexp, 'grr$1') } else if (/^[1-9]/.test(word)) { return word } else return 'gr' + word.slice(1)
+}
+
+function paragrapheToDino (node) {
+    if (node.nodeType === Node.TEXT_NODE) {
+        const content = []
+        for (const word of node.textContent.split(/\s/)) {
+            if (word.length === 0) continue
+            const translatedWord = wordToDino(word)
+            content.push(translatedWord)
+        }
+        content.push('\n')
+        content[0] = content[0].slice(0, 1).toUpperCase() + content[0].slice(1)
+        node.textContent = content.join(' ')
+    } else {
+        node.childNodes.forEach(paragrapheToDino)
+    }
+}
+
+async function titleToDinos (node) {
+    const res = await chrome.runtime.sendMessage({
+        type: 'get_dinos_title'
+    })
+    if (res) node.textContent = res
+}
+
+document.querySelectorAll('p, a').forEach(node => getTextNode(node, paragrapheToDino))
+document.querySelectorAll('h1, h2, h3, h4').forEach(node => getTextNode(node, titleToDinos))
