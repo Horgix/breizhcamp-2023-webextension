126b231b3036262bf3ff8f5f69dca0a954b71d92
Aurélien Partiot <aurelien.partiot@orange.com>
2023-06-26 16:00:18 +0200

    fix: Remove usless files

--- tradino/02_service-worker/src/content-script/tradinos.js
+++
@@ -1,6 +0,0 @@
-export default async function translateToDinos (node) {
-    const res = await chrome.runtime.sendMessage({ type: 'translate_to_dinos' })
-    if (res) {
-        node.textContent = res
-    }
-}
--- tradino/04_storage/src/background/utils.js
+++
@@ -1,6 +0,0 @@
-import lyrics from '../data/dinos.json'
-
-export function getRandomTitle () {
-    const lyricIndex = Math.floor(Math.random() * lyrics.length)
-    return lyrics[lyricIndex]
-}
