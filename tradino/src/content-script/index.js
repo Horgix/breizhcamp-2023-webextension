87413dcb9d104c34992d021dd93471834d12b767
Aurélien Partiot <aurelien.partiot@orange.com>
2023-06-26 17:15:04 +0200

    update: Add ready state content-script

--- tradino/src/content-script/index.js
+++ tradino/src/content-script/index.js
@@ -1,2 +1,11 @@
-console.log('Disneyland aussi a eu quelques difficultés à ses débuts !\n- Oui mais, John, quand les pirates des Caraïbes se détraquaient, ils ne dévoraient pas les touristes !\n\n-- Jurassic Parc, 1993')
-
+const html = `
+<h1>Ready !</h1>
+<img style="width: 600px" src='${chrome.runtime.getURL('data/jurassic-park.jpg')}' />
+<p>
+Disneyland aussi a eu quelques difficultés à ses débuts !<br>
+- Oui mais, John, quand les pirates des Caraïbes se détraquaient, ils ne dévoraient pas les touristes !<br>
+<br>
+-- Jurassic Parc, 1993
+</p>
+`
+document.body.innerHTML = html
