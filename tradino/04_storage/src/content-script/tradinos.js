53245ef3249b0733933c5b7ac6460e2db6097ba8
Aurélien Partiot <aurelien.partiot@orange.com>
2023-06-26 15:28:03 +0200

    update: Set branch

--- tradino/04_storage/src/content-script/tradinos.js
+++
@@ -1,8 +0,0 @@
-export default async function translateToDinos (node) {
-    const res = await chrome.runtime.sendMessage({ type: 'translate_to_dinos' })
-    if (res) {
-        node.textContent = res
-        return 1
-    }
-    return 0
-}
